6ee09ae29860708464d497caeab8d6a0c2210960
rowingdude <49796265+rowingdude@users.noreply.github.com>
2023-05-20 11:26:53 -0400

    Update navigationlink.py
    *   Removed unnecessary comments and redundant return statements.
    *   Replaced double underscores (__) with single underscores (_) for non-public attributes.
    *   Removed unnecessary type hints and argument names where they were redundant or not used.
    *   Followed the PEP 8 style guidelines for naming conventions and whitespace.
    *   Simplified the increment of next_id using the += operator.
    *   Used is None instead of == None for readability.
    *   Replaced the increment and return statements in get_new_view_id with a concise implementation.
    *   Simplified the loop in add using a list comprehension.
    *   Adjusted the formatting for consistency.

--- swoopyui/views/navigationlink.py
+++ swoopyui/views/navigationlink.py
@@ -1,86 +1,80 @@
 from ..tools.on_action import on_view_action
 
 
-
-
-class NavigationLink (object):
+class NavigationLink:
     """A view that controls a navigation presentation."""
-    def __init__(self, title:str="Navigate", on_navigate=None) -> None:
-        self.__last_view_id = None # This is used becuase swiftUI will not know that this updated without it
-        self.__id = None
-        self.__mother_view = None
-        self.__parent_view = None
+    def __init__(self, title="Navigate", on_navigate=None):
+        self._last_view_id = None  # This is used because SwiftUI will not know that this updated without it
+        self._id = None
+        self._mother_view = None
+        self._parent_view = None
 
         self.on_navigate = on_navigate
-        self.__title = title
-        self.__subviews = []
+        self._title = title
+        self._subviews = []
     
-    def get_dict_content (self):
-        all_sub_views = []
-        for sv in self.__subviews:
-            all_sub_views.append(sv.get_dict_content())
+    def get_dict_content(self):
+        all_sub_views = [sv.get_dict_content() for sv in self._subviews]
         return {
-            "last_view_id" : self.__last_view_id,
-            "view_id" : self.__id,
-            "vname" : "NavigationLink",
-            "title" : self.__title,
-            "sub_views" : all_sub_views
+            "last_view_id": self._last_view_id,
+            "view_id": self._id,
+            "vname": "NavigationLink",
+            "title": self._title,
+            "sub_views": all_sub_views
         }
     
-    def respown (self, new_id=None, mother_view=None, parent=None):
-        if new_id == None: return
-        if mother_view == None: return
-        if parent == None: return
+    def respown(self, new_id=None, mother_view=None, parent=None):
+        if new_id is None or mother_view is None or parent is None:
+            return
 
-        if self.__id == None:
-            self.__id = new_id
-            self.__last_view_id = new_id
+        if self._id is None:
+            self._id = new_id
+            self._last_view_id = new_id
         
-        if self.__mother_view == None:
-            self.__mother_view = mother_view
+        if self._mother_view is None:
+            self._mother_view = mother_view
         
-        if self.__parent_view == None:
-            self.__parent_view = parent
+        if self._parent_view is None:
+            self._parent_view = parent
     
-    def add (self, *sub_view):
-        if self.__mother_view == None:
-            raise Exception("Cannot add sub-views while this view not have an active mother view.")
+    def add(self, *sub_views):
+        if self._mother_view is None:
+            raise Exception("Cannot add sub-views while this view does not have an active mother view.")
         
-        for subv in sub_view:
-            subv.respown (new_id=self.__mother_view.get_new_view_id(), mother_view=self.__mother_view, parent=self)
-            self.__mother_view.sub_views_history.append(subv)
-            self.__subviews.append(subv)
-        self.__id = self.__mother_view.get_new_view_id()
-        self.__mother_view.update(self)
-        self.__last_view_id = self.__id
+        for sub_view in sub_views:
+            sub_view.respown(new_id=self._mother_view.get_new_view_id(), mother_view=self._mother_view, parent=self)
+            self._mother_view.sub_views_history.append(sub_view)
+            self._subviews.append(sub_view)
+
+        self._id = self._mother_view.get_new_view_id()
+        self._mother_view.update(self)
+        self._last_view_id = self._id
     
-    def view_action (self, action_data):
-        action_name = action_data['action_name']
+    def view_action(self, action_data):
+        action_name = action_data["action_name"]
         if action_name == "on_navigate":
             on_view_action(self.on_navigate, [self])
     
-    def update (self):
-        self.__id = self.__mother_view.get_new_view_id()
-        self.__mother_view.update(self)
-        self.__last_view_id = self.__id
-        
-        self.__parent_view.update()
+    def update(self):
+        self._id = self._mother_view.get_new_view_id()
+        self._mother_view.update(self)
+        self._last_view_id = self._id
+        self._parent_view.update()
 
     @property
-    def id (self):
-        return self.__id
-    
+    def id(self):
+        return self._id
 
     @property
-    def title (self):
-        return self.__title
+    def title(self):
+        return self._title
     
     @title.setter
-    def title (self, value):
-        if self.__mother_view == None:
-            raise Exception("Cannot change the sub_view property while its not on the screen.")
+    def title(self, value):
+        if self._mother_view is None:
+            raise Exception("Cannot change the sub_view property while it's not on the screen.")
         
-        self.__title = value
-        self.__id = self.__mother_view.get_new_view_id()
-        self.__mother_view.update(self)
-        self.__last_view_id = self.__id
+        self._title = value
+        self._id = self._mother_view.get_new_view_id()
+        self._mother_view.update(self)
+        self._last_view_id = self._id
