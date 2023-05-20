596bf2dfca873887ab32cd806ffaa4c1c7199f1b
Kot <86029286+SKbarbon@users.noreply.github.com>
2023-05-20 15:59:47 +0300

    Update the way  python run swiftUI to be efficient

--- swoopyui/tools/run_swiftUI.py
+++ swoopyui/tools/run_swiftUI.py
@@ -1,34 +1,24 @@
-from .unzip_assets import unzip_file
-import requests
-import tempfile
-import shutil
 import subprocess
 import os
+import shutil
+from .unzip_assets import unzip_file
 
-def run_swiftUI_app (port, tmp_dir):
-    # return
-    from .unzip_assets import unzip_file
-    import subprocess
-    import os
-
-    # Create a temporary directory.
-
+def run_swiftUI_app(port, tmp_dir):
     temp_dir = tmp_dir
+    print(f"tmp exist: {os.path.isdir(temp_dir)}")
 
-    # Generate the path that the app will be in.
     zip_path = str(__file__).replace("tools/run_swiftUI.py", "assets/swoopyui.zip")
-
     new_app_path = os.path.join(temp_dir, "swoopyui.app/")
 
-    unzip_file (zip_path=zip_path, destination_path=temp_dir)
+    unzip_file(zip_path=zip_path, destination_path=temp_dir)
 
-    # Start the subprocess to run the app
+    executable_of_the_app = os.path.join(new_app_path, "Contents/MacOS/swoopyui")
+    chmod_command = ["chmod", "+x", executable_of_the_app]
 
-    executble_of_the_app = os.path.join(new_app_path, "Contents/MacOS/")
+    subprocess.run(chmod_command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-    run_command = r"./swoopyui"
-    subprocess.run(f"cd {executble_of_the_app}\nchmod +x swoopyui\n{run_command} {port}", shell=True, stdout=None)
+    run_command = [executable_of_the_app, str(port)]
+    subprocess.run(run_command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-    # delete the temp after done using the app
     print(temp_dir)
     shutil.rmtree(temp_dir)
