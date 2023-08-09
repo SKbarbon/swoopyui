b16ad0b743f5b67b2013d2d405a0e4d09de90198
Kot <86029286+SKbarbon@users.noreply.github.com>
2023-08-09 13:52:51 +0300

    Updates

---
+++ swoopyui_xcode/swoopyui/AllViews/TheIconView.swift
@@ -0,0 +1,18 @@
+//
+//  TheIconView.swift
+//  swoopyui
+//
+//  Created by Yousif Aladwani on 09/08/2023.
+//
+
+import SwiftUI
+
+struct TheIconView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TheIconView()
+}
