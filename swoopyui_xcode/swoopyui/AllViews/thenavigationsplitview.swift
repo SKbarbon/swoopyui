6490baac906d9dd8fc0f877d72de2e5f4c964e7b
Kot <86029286+SKbarbon@users.noreply.github.com>
2023-08-09 15:15:47 +0300

    u

---
+++ swoopyui_xcode/swoopyui/AllViews/thenavigationsplitview.swift
@@ -0,0 +1,18 @@
+//
+//  thenavigationsplitview.swift
+//  swoopyui
+//
+//  Created by Yousif Aladwani on 09/08/2023.
+//
+
+import SwiftUI
+
+struct thenavigationsplitview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    thenavigationsplitview()
+}
