66534654283a217501a2010fe95ccfb32419581c
Kot <86029286+SKbarbon@users.noreply.github.com>
2023-08-09 16:40:32 +0300

    U

--- swoopyui_xcode/swoopyui copy/AllViews/TheNavigationLink.swift
+++ swoopyui_xcode/swoopyui copy/AllViews/TheNavigationLink.swift
@@ -6,24 +6,14 @@ struct TheNavigationLink: View {
     @State var host_port : Int
     @State var textData : SwoopyView
     var body: some View {
-        Button {
-            isNavPresented = true
-            ClientSideUpdateReq(hostPort: host_port, update_name: "on_view_action", update_content: [
-                "action_name" : "on_navigate",
-                "view_id" : textData.view_id
-            ])
-        } label: {
-            ForEach(textData.skin_views!, id: \.view_id) { sk_view in
-                GetTheDataView(swoopyuiViewData: sk_view, hostPort: host_port)
-            }
-        }
-        .navigationDestination(isPresented: $isNavPresented) {
+        NavigationLink (destination: VStack {
             ForEach(textData.sub_views!, id: \.view_id) { sub_view in
                 GetTheDataView(swoopyuiViewData: sub_view, hostPort: host_port)
             }
-        }
-        .onAppear() {
-            isNavPresented = textData.is_presented ?? false
+        }, isActive: $isNavPresented) {
+            ForEach(textData.skin_views!, id: \.view_id) { sub_view in
+                GetTheDataView(swoopyuiViewData: sub_view, hostPort: host_port)
+            }
         }
     }
 }
