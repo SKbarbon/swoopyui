eb9ced1a2eb6b92c9ac59d651dcccc14fd6fddce
Kot <86029286+SKbarbon@users.noreply.github.com>
2023-09-15 17:53:48 +0300

    upgrade setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name='swoopyui',
-    version='2.1',
+    version='2.2',
     author='SKbarbon',
     description='A python library that allow you to build swiftUI apps using python.',
     long_description=long_des,
